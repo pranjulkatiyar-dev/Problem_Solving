dbe37195cadf8ce1f1fab6b23d089a61595680f6
Pranjul Katiyar <pranjulkatiyar449@gmail.com>
2026-04-20 23:12:37 +0530

    Time: 0 ms (100%), Space: 8.5 MB (49.61%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0 || x == 1) return x;
+        
+        int left = 1, right = x;
+        int ans = 0;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if ((long long)mid * mid == x) {
+                return mid;
+            }
+            else if ((long long)mid * mid < x) {
+                ans = mid;       // store possible answer
+                left = mid + 1;  // move right
+            }
+            else {
+                right = mid - 1; // move left
+            }
+        }
+        
+        return ans;
+    }
+};
